7234af042006110b281102bea1394a0bbf881c47
Alejandro Santiago <cubanfireball81@gmail.com>
2022-03-16 18:01:36 -0800

    Updates code to work on modern browsers

--- pacmen.js
+++ pacmen.js
@@ -23,8 +23,8 @@ function makePac() {
     newimg.style.position = 'absolute';
     newimg.src = './images/PacMan1.png';
     newimg.width = 100;
-    newimg.style.left = position.x;
-    newimg.style.top = position.y;
+    newimg.style.left = position.x + 'px';
+    newimg.style.top = position.y + 'px';
     game.appendChild(newimg);
     // new style of creating an object
     return {
@@ -41,8 +41,8 @@ function update() {
         item.position.x += item.velocity.x;
         item.position.y += item.velocity.y;
 
-        item.newimg.style.left = item.position.x;
-        item.newimg.style.top = item.position.y;
+        item.newimg.style.left = item.position.x + 'px';
+        item.newimg.style.top = item.position.y + 'px';
     })
     setTimeout(update, 20);
 }
